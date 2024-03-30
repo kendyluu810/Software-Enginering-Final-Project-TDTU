f9db414b3e35cbc9e3a2bc9c3aeedd01add9715a
Luu Hung <73063097+kendyluu810@users.noreply.github.com>
2024-03-30 11:37:55 +0700

    Update CSMS.sql

--- CSMS.sql
+++ CSMS.sql
@@ -15,17 +15,17 @@ Select * from users
 INSERT INTO users (username,password,role,status,profile_image,date_reg) VALUES ('admin','Admin@123','Admin','Active','','2024-02-02')
 
 CREATE TABLE products (
-    id INT PRIMARY KEY,
-    prod_id NVARCHAR(50),
-    prod_name NVARCHAR(100),
-    prod_type NVARCHAR(50),
-    prod_stock NVARCHAR(50),
-    prod_price NVARCHAR(50),
-    prod_status NVARCHAR(50),
-    prod_image NVARCHAR(200),
-    date_insert NVARCHAR(50),
-    date_update NVARCHAR(50),
-    date_delete NVARCHAR(50)
+    id INT PRIMARY KEY IDENTITY(1,1),
+    prod_id NVARCHAR(MAX) NULL,
+    prod_name NVARCHAR(MAX) NULL,
+    prod_type NVARCHAR(MAX) NULL,
+    prod_stock INT NULL,
+    prod_price FLOAT NULL,
+    prod_status NVARCHAR(MAX) NULL,
+    prod_image NVARCHAR(MAX) NULL,
+    date_update DATE NULL,
+    date_insert DATE NULL,
+    date_delete DATE NULL
 );
 
 CREATE TABLE customers (
